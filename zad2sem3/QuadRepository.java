ea28b2409f1408ed7c189bf8d11ac1492c374ac8
lukaszaleks <115348192+lukaszaleks@users.noreply.github.com>
2024-04-21 10:34:27 +0200

    Delete zad2sem3 directory

--- zad2sem3/QuadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.zad1sem3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuadRepository extends JpaRepository<Quad, Long> {
-}
